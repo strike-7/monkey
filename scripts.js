175a96e8e60268638bb6e1c5db7f79536402308d
strike-7 <ehenriq21@gmail.com>
2024-07-01 20:34:48 -0400

    file uploads

---
+++ scripts.js
@@ -0,0 +1,33 @@
+$('#yeas').on('click', () => {
+    $('#water-buttons').toggle()
+    $('#aqua').toggle();
+    $('#idby').show('1000')
+});
+
+$('#aqua-next').on('click', () =>{
+    $('#question1').toggle();
+    $('#question1').next().toggle('slow')
+})
+$('#watermelon').on('click', () => {
+    alert('are you sureeeeeee monkey???')
+})
+$('#banana').on('click', () => {
+    $('#fruit-buttons').toggle()
+    $('#fruits').toggle();
+    $('#hmm').show('slow')
+})
+
+$('#ban-but').on('click', () => {
+    $('#question2').toggle();
+    $('#question2').next().toggle('slow')
+})
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function randomDiss() {
+    let x = ["Dont lie bruh, its literally a selfie you sent me...", "I think your picking the wrong answer on purpose now...",
+        "umm okay, lets be real now..."]
+        alert(x[getRandomInt(3)])
+
+}
